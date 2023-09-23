15de0e11ab8c1f3769655b3b48da0a06f3722fdc
Nam <namdeveloper.ca@gmail.com>
2023-09-23 14:45:02 +0700

    Fetch all parents posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,60 @@
-import { UserButton } from '@clerk/nextjs'
+import ThreadCard from '@/components/cards/ThreadCard'
+import { fetchPosts } from '@/lib/actions/thread.actions'
+import { fetchUser } from '@/lib/actions/user.actions'
+import { UserButton, currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+type post = {
+  _id: string
+  currentUserId: string
+  parentId: string
+  text: string
+  author: { name: string; image: string; id: string }
+  community: { id: string; name: string; image: string }
+  createdAt: string
+  children: { author: { image: string } }[]
+}
+
+export default async function Home() {
+  const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect('/onboarding')
+
+  const result = await fetchPosts(1, 30)
+
   return (
     <>
-      {/* <h1 className='head-text text-left'>Home</h1> */}
-      <div>
-        <UserButton afterSignOutUrl='/' />
-      </div>
+      <h1 className='text-left head-text'>Home</h1>
+
+      <section className='flex flex-col gap-10 mt-9'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      {/* <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
     </>
   )
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -42,3 +42,40 @@ export async function createThread({ text, author, communityId, path }: Params) 
     throw new Error(`Failed to create thread: ${error.message}`)
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB()
+
+  // calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize
+
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: 'author',
+      model: User,
+    })
+    .populate({
+      path: 'community',
+      model: Community,
+    })
+    .populate({
+      path: 'children', // Populate the children field
+      populate: {
+        path: 'author', // Populate the author field within children
+        model: User,
+        select: '_id name parentId image', // Select only _id and username fields of the author
+      },
+    })
+
+  const totalPostCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+
+  const posts = await postsQuery.exec()
+
+  const isNext = totalPostCount > skipAmount + posts.length
+
+  return { posts, isNext }
+}
