89a523589f42be22a4a243da1797396bd2151236
Nam <namdeveloper.ca@gmail.com>
2023-09-23 16:50:18 +0700

    Onboarding: Get User from DB

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,6 +1,8 @@
 import AccountProfile from '@/components/forms/AccountProfile'
 import React from 'react'
 import { currentUser } from '@clerk/nextjs'
+import { fetchUser } from '@/lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 
 type userType = {
   id: string
@@ -14,33 +16,28 @@ type userType = {
 const Page = async () => {
   // user get from clerk
   const user = await currentUser()
+  if (!user) return null // to avoid typescript warnings
 
-  // user get from database
-  const userInfo = {
-    _id: '',
-    username: '',
-    name: '',
-    bio: '',
-    image: '',
-  }
+  const userInfo = await fetchUser(user.id)
+  if (userInfo?.onboarded) redirect('/')
 
-  const userData: userType = {
-    id: user?.id || '',
+  const userData = {
+    id: user.id,
     objectId: userInfo?._id,
-    username: userInfo?.username || user?.username || '',
-    name: userInfo?.name || user?.firstName || '',
-    bio: userInfo?.bio || '',
-    image: userInfo?.image || user?.imageUrl || '',
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? '',
+    bio: userInfo ? userInfo?.bio : '',
+    image: userInfo ? userInfo?.image : user.imageUrl,
   }
 
   return (
-    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+    <main className='flex flex-col justify-start max-w-3xl px-10 py-20 mx-auto'>
       <h1 className='head-text'>Onboarding</h1>
       <p className='mt-3 text-base-regular text-light-2'>
         Complete your profile now, to use Threds.
       </p>
 
-      <section className='mt-9 bg-dark-2 p-10'>
+      <section className='p-10 mt-9 bg-dark-2'>
         <AccountProfile user={userData} btnTitle='Submit' />
       </section>
     </main>
